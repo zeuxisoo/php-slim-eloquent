de4049c59f6880d40433f290a538f3125e7be8ca
Zeuxis Lo <seekstudio@gmail.com>
2015-05-26 19:18:01 +0800

    add eloquent package and finish prototype middleware

--- src/Zeuxisoo/Laravel/Database/Eloquent/ModelMiddleware.php
+++ src/Zeuxisoo/Laravel/Database/Eloquent/ModelMiddleware.php
@@ -2,11 +2,36 @@
 namespace Zeuxisoo\Laravel\Database\Eloquent;
 
 use Slim\Middleware;
+use Illuminate\Database\Capsule\Manager as Capsule;
 
 class ModelMiddleware extends Middleware {
 
+    private function makeCapsule() {
+        $capsule = new Capsule;
+        $capsule->addConnection([
+            'driver'    => 'mysql',
+            'host'      => 'localhost',
+            'database'  => 'test',
+            'username'  => 'root',
+            'password'  => '',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_general_ci',
+            'prefix'    => ''
+        ]);
+
+        $capsule->setAsGlobal();
+        $capsule->bootEloquent();
+
+        return $capsule;
+    }
+
     public function call() {
-        $app = $this->app;
+        $app     = $this->app;
+        $capsule = $this->makeCapsule();
+
+        $app->container->singleton('db', function() use ($capsule) {
+            return $capsule;
+        });
 
         $this->next->call();
     }
